31ba3f511ef5b778b73c51c5c765cf2041978264
MC <matthewcrick@outlook.com>
2023-05-24 12:44:17 +1000

    Added example test case

--- main.py
+++ main.py
@@ -115,3 +115,15 @@ def calculateMaxDataFlow(adjMatrix, origin):
         max_data_flow += flow
 
     return max_data_flow
+
+
+# Example
+connections = [(0, 1, 3000), (1, 2, 2000), (1, 3, 1000), (0, 3, 2000), (3, 4, 2000), (3, 2, 1000)]
+maxIn = [5000, 3000, 3000, 3000, 2000]
+maxOut = [5000, 3000, 3000, 2500, 1500]
+origin = 0
+targets = [4, 2]
+# Your function should return the maximum possible data throughput from the
+# data centre origin to the data centres specified in targets.
+print(maxThroughput(connections, maxIn, maxOut, origin, targets))
+#4500
